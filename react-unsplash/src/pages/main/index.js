e9e18d9f19b3ffcf871fb36d5d3a869f6531f6c6
Yassui <yassuifortes@outlook.com>
2020-05-13 19:49:07 -0300

    correção de rota e auth

--- react-unsplash/src/pages/main/index.js
+++ react-unsplash/src/pages/main/index.js
@@ -2,17 +2,28 @@ import React, { Component } from 'react';
 import api from '../../services/api';
 
 export default class Main extends Component {
+    state = {
+      listImages: []
+    };
+
     componentDidMount() {
        this.loadImages(); 
     }
     loadImages = async () => {
-      const response = await api.get('/');
-
-      console.log(response);
+     
+      const response = await api.get('https://api.unsplash.com/photos/random?per_page=5&client_id=3cnwPgyGfWgVkZ5UhUvqdlc1qL-kBR4PM3xYQr_Ipa4');
+       
+      this.setState({ listImages: response.data });
     };
 
     render() {
-        return <h1>Hello</h1>;
+        return (
+          <div className="images-list">
+            {this.state.listImages.map(listImages => (
+              <h2>{listImages.user}</h2>
+            ))}
+          </div>
+        )
         
     }
 } 
--- react-unsplash/src/services/api.js
+++ react-unsplash/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const YOUR_ACCESS_KEY = '3cnwPgyGfWgVkZ5UhUvqdlc1qL-kBR4PM3xYQr_Ipa4'; 
+//let YOUR_ACCESS_KEY = '3cnwPgyGfWgVkZ5UhUvqdlc1qL-kBR4PM3xYQr_Ipa4'; 
 
-const api = axios.create(`https://api.unsplash.com/photos?client_id=${YOUR_ACCESS_KEY}`);
+const api = axios.create({ baseURL: 'https://api.unsplash.com/photos'});  
+
 
 export default api;
