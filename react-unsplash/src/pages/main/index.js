f17e3f7d3e4822a007098a20e79ff82f668f7594
Yassui <yassuifortes@outlook.com>
2020-05-15 16:37:42 -0300

    state.listImages

--- react-unsplash/src/pages/main/index.js
+++ react-unsplash/src/pages/main/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import api from '../../services/api';
 
 export default class Main extends Component {
-    state = {
+    state =  {
       listImages: []
     };
 
@@ -10,20 +10,20 @@ export default class Main extends Component {
        this.loadImages(); 
     }
     loadImages = async () => {
-     
-      const response = await api.get('https://api.unsplash.com/photos/random?per_page=5&client_id=3cnwPgyGfWgVkZ5UhUvqdlc1qL-kBR4PM3xYQr_Ipa4');
-       
+      const key_api = '3cnwPgyGfWgVkZ5UhUvqdlc1qL-kBR4PM3xYQr_Ipa4';
+      const response = await api.get(`https://api.unsplash.com/photos/random?per_page=5&client_id=${key_api}`);
+      console.log(response.data.user)
       this.setState({ listImages: response.data });
+      
     };
 
     render() {
         return (
-          <div className="images-list">
-            {this.state.listImages.map(listImages => (
-              <h2>{listImages.user}</h2>
-            ))}
+          <div className="list-images">
+            { this.state.listImages.map(listImages => 
+              <h2 key="listImages.id">{listImages.user}</h2>
+            )}
           </div>
         )
-        
     }
 } 
